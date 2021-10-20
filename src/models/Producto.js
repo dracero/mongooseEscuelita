e9758a9d37c4365c109a527552ef51fa82290620
dracero <diego.racero@gmail.com>
2021-10-20 20:09:39 -0300

    Update Producto.js

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -14,36 +14,45 @@ class Producto{
     }
     
     async getAll(){
-        
+        try {
         const productos = productMD.find()
         return productos
-      
+        } catch (err) {
+            throw err
+        }
     }
 
     async save(data){
-
+        try {
         const producto = await productMD.create(data)
         return producto
-       
+        } catch (err) {
+            throw err
+        }
     }
 
     async update(id, producto){
-        
+        try {
         const updated  = await productMD.updateOne({_id: id},{$set: producto})
         console.log(updated)
         if(!updated) return undefined
         
         return this.get(id)
-            
+        } catch (err) {
+            throw err
+        }   
        
     }
     async delete(id){
+         try{
          const product = await this.get(id)
-            
          if(!product) return undefined
  
          await productMD.deleteOne({ _id: product._id})
          return product
+         } catch (err) {
+            throw err
+        }     
        
     }
 }
